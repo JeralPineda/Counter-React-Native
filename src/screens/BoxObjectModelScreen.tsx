5f62146389fb5cbf776be36b179cd0484abf71b1
Jeral Pineda <jpineda@banhcafe.hn>
2023-07-18 08:57:42 -0600

    feat: Uso de Padding, margin, border, width y height

--- src/screens/BoxObjectModelScreen.tsx
+++ src/screens/BoxObjectModelScreen.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 const BoxObjectModelScreen = () => {
   return (
-    <View>
-      <Text>Box Object Model</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Box Object Model</Text>
     </View>
   );
 };
 
 export default BoxObjectModelScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  title: {
+    fontSize: 20,
+    borderWidth: 2,
+    padding: 10, //unidades //padding 4 lados
+    // paddingHorizontal: 2, //padding left y right
+    // paddingVertical: 2, //padding top y bottom
+    // paddingLeft: 2,
+    // paddingRight: 2
+    // paddingTop: 2,
+    // paddingBottom: 2,
+    marginHorizontal: 20,
+
+    // backgroundColor: 'red',
+  },
+});
