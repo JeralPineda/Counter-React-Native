69c30f6e47a2a429b509ee1ee29d9d4a8cc30509
Jeral Pineda <jpineda@banhcafe.hn>
2023-07-18 08:21:41 -0600

    feature: Creando componente para Box Object Model

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import CounterScreen from './screens/CounterScreen';
+
+// import CounterScreen from './screens/CounterScreen';
+import BoxObjectModelScreen from './screens/BoxObjectModelScreen';
+import {SafeAreaView} from 'react-native';
 
 function App(): JSX.Element {
-  return <CounterScreen />;
+  return (
+    <SafeAreaView>
+      {/* SafeAreaView - Muestra correctamente la información cuando tiene un notch (iphone),
+      evita que choque y es raro ponerlo al nivel del app */}
+      <BoxObjectModelScreen />
+    </SafeAreaView>
+  );
 }
 
 export default App;
