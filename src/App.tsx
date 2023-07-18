128eb96a7d8bd8e00b46860e7fc0be08da481b50
Jeral Pineda <jpineda@banhcafe.hn>
2023-07-18 08:41:39 -0600

    styles: Agregando estilo al contenedor del app

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 // import CounterScreen from './screens/CounterScreen';
 import BoxObjectModelScreen from './screens/BoxObjectModelScreen';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       {/* SafeAreaView - Muestra correctamente la información cuando tiene un notch (iphone),
       evita que choque y es raro ponerlo al nivel del app */}
       <BoxObjectModelScreen />
@@ -15,3 +15,9 @@ function App(): JSX.Element {
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, //Hace que se expanda en toda la pantalla
+  },
+});
