3f90988c76bbfec81922889162776e71a52fef3a
Jeral Pineda <jpineda@banhcafe.hn>
2023-07-21 08:22:13 -0600

    fix: Height, Width porcentual y dimensiones de la pantalla

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
 
 // import CounterScreen from './screens/CounterScreen';
-import BoxObjectModelScreen from './screens/BoxObjectModelScreen';
-import {SafeAreaView, StyleSheet} from 'react-native';
+// import BoxObjectModelScreen from './screens/BoxObjectModelScreen';
+import DimensionsScreen from './screens/DimensionsScreen';
 
 function App(): JSX.Element {
   return (
     <SafeAreaView style={styles.container}>
       {/* SafeAreaView - Muestra correctamente la información cuando tiene un notch (iphone),
       evita que choque y es raro ponerlo al nivel del app */}
-      <BoxObjectModelScreen />
+      <DimensionsScreen />
     </SafeAreaView>
   );
 }
---
+++ src/screens/DimensionsScreen.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {StyleSheet, View, Text, useWindowDimensions} from 'react-native';
+
+const DimensionsScreen = () => {
+  // const {width, height} = Dimensions.get('window');
+
+  const {width, height} = useWindowDimensions();
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <View style={{...styles.purpleBox, width: width * 0.6}} />
+        <View style={styles.orangeBox} />
+      </View>
+
+      <Text style={styles.title}>
+        W: {width}, H: {height}
+      </Text>
+    </View>
+  );
+};
+
+export default DimensionsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 200,
+    backgroundColor: 'red',
+  },
+  purpleBox: {
+    backgroundColor: '#5856d6',
+    // width: '50%',
+    height: '50%',
+  },
+  orangeBox: {
+    backgroundColor: '#f0a23b',
+    // width: '50%',
+    // height: '50%',
+  },
+
+  title: {
+    fontSize: 30,
+    textAlign: 'center',
+  },
+});
