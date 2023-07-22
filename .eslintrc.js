0e920baef3ef97860d7dabecee9e6cc8a3b730f7
Jeral Pineda <jeral.cris@gmail.com>
2023-07-22 10:41:01 -0600

    fix: configuranto slint y pretier para comillas dobles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: "@react-native",
   rules: {
-    'prettier/prettier': 0,
+    "prettier/prettier": 0,
+    quotes: [2, "double"],
   },
 };
